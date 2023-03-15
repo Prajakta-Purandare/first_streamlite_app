9d3f1a18f7cb86a3a8c2402d6675d546390d5939
Prajakta-Purandare <126728608+Prajakta-Purandare@users.noreply.github.com>
2023-03-14 21:42:50 -0400

    Update stream_app.py
    added tabular CSV data

--- stream_app.py
+++ stream_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My parents new healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -6,3 +7,6 @@ streamlit.text('Oatmeal with fruites')
 streamlit.text('Green Smoothie')
 streamlit.text('Hard boiled eggs')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
